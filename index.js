b35f7d4fbff587100cbd289706f5c06a2f171178
Cédric HUMBERT <cedric.humbert3@gmail.com>
2018-12-06 22:43:05 +0100

    Indent switch
    Enforcing code style wont hurt !

--- index.js
+++ index.js
@@ -10,42 +10,42 @@ let liekana;
 document.getElementById("calcul").addEventListener("click", function( event ) {
 
   console.log(event.target.value);
+  
   switch (event.target.value) {
     case "Sum":
-    suma=pirmaReiksme+antraReiksme;
-    document.getElementById("ats").innerHTML=suma;
+      suma=pirmaReiksme+antraReiksme;
+      document.getElementById("ats").innerHTML=suma;
       console.log(`I will sum: ${suma}`);
       break;
-      case "Subtract":
+    case "Subtract":
       atimtis=pirmaReiksme-antraReiksme;
       document.getElementById("ats").innerHTML=atimtis;
-        console.log(`i will subtract ${atimtis}`);
-        break;
-        case "Divide":
-        dalyba=pirmaReiksme/antraReiksme;
-        document.getElementById("ats").innerHTML=dalyba;
-          console.log(`i will divide ${dalyba}`);
-          break;
-          case "Multiply":
-          daugyba=pirmaReiksme*antraReiksme;
-          document.getElementById("ats").innerHTML=daugyba;
-            console.log(`i will multiply ${daugyba}`);
-            break;
-            case "Modus":
-            liekana=pirmaReiksme%antraReiksme;
-            document.getElementById("ats").innerHTML=liekana;
-              console.log(`i will mod ${liekana}`);
-              break;
-              case "Clear":
-              console.log("trinu");
-              pirmaReiksme='';
-              antraReiksme='';
-              document.getElementById("pirmasSk").value='';
-              document.getElementById("antrasSk").value='';
-              document.getElementById("ats").innerHTML='';
-              break;
+      console.log(`i will subtract ${atimtis}`);
+      break;
+    case "Divide":
+      dalyba=pirmaReiksme/antraReiksme;
+      document.getElementById("ats").innerHTML=dalyba;
+      console.log(`i will divide ${dalyba}`);
+      break;
+    case "Multiply":
+      daugyba=pirmaReiksme*antraReiksme;
+      document.getElementById("ats").innerHTML=daugyba;
+      console.log(`i will multiply ${daugyba}`);
+      break;
+    case "Modus":
+      liekana=pirmaReiksme%antraReiksme;
+      document.getElementById("ats").innerHTML=liekana;
+      console.log(`i will mod ${liekana}`);
+      break;
+    case "Clear":
+      console.log("trinu");
+      pirmaReiksme='';
+      antraReiksme='';
+      document.getElementById("pirmasSk").value='';
+      document.getElementById("antrasSk").value='';
+      document.getElementById("ats").innerHTML='';
+      break;
     default:
-
   }
   }, false);
 
